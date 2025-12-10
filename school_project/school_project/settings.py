4df22f2594385ed773a04fb1a4eca93011135ce3
IvanArno <ivan.arnautovskii@gmail.com>
2025-12-10 09:41:51 +0300

    Ver_web1

--- school_project/school_project/settings.py
+++ school_project/school_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-e=b2$j4k*00zvqf!774tnk2x43wpvcgqvh_s41r1(z)8qki5qb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['school-project.loca.lt']
+ALLOWED_HOSTS = ['school-project.loca.lt',
+                 '127.0.0.1',]
 
 
 # Application definition
